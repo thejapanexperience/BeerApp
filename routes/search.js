c8d636afa2265367c5cbe5d146261787af0ac051
Sammi A <sammi.m.yin@gmail.com>
2016-10-11 21:46:03 -0700

    workinggit add .

--- routes/search.js
+++ routes/search.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Twitter = require('twitter')
+require("dotenv").config()
 
 const client = new Twitter({
-  consumer_key: 'MxIk4vCciG1vx8Jjt5WilKB0G',
-  consumer_secret: 'GDs0Wookr0AQVkf8yrU1ygyfSuoJDKCOUYzYlkMoQkOY6zXWNn',
-  access_token_key: '755419386945544195-EdeVW436hFmAxBvQOd5ykASOg7SI9RE',
-  access_token_secret: 'Or2eBH807aMA8g0GyRREttOzfuSiElkKZemeM1oz1N25Q'
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret ,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
 });
 
 router.get('/:topic',(req,res) => {
   let {topic} = req.params;
-  console.log('topic: ', topic);
   client.get('search/tweets', {q: `${topic}`}, function(error, tweets, response) {
-     console.log("response",tweets);
      if (error) return res.status(400).send(error);
      res.send(tweets.statuses);
   });
